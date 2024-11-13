c852c8d654d9b544604e7d7cea00d67dd8916533
joelb-ps <111363018+joelb-ps@users.noreply.github.com>
2024-11-13 17:10:00 +1100

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ elif page == "Detailed Funnel Analysis":
         </iframe>
         ''', unsafe_allow_html=True)
      html_file_url5 = "https://joelb-ps.github.io/mosh-streamlit/time_at_step_1.html"
-    st.markdown(f'''
+     st.markdown(f'''
         <iframe src="{html_file_url5}" width="120%" height="600px" 
             style="border:none; transform: scale(0.6); transform-origin: 0 0;">
         </iframe>
